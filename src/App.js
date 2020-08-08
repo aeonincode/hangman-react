f222e2f8314d985caf7f3865b6f5e02e2862baba
aeonincode <khashayar.sasannia@hotmail.com>
2020-08-08 13:08:22 +0200

    build components and implement state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+
 import './App.css';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
-  return <div className='App'></div>;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setwrongLetters] = useState([]);
+
+  return (
+    <>
+      <Header />
+      <div className='game-container'>
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
+  );
 }
 
 export default App;
